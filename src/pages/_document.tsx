ada6ce89339b2e631a7b74a1fc720365db8c05f1
Aarish <aarisharshi123@gmail.com>
2025-07-27 14:35:55 +0530

    Added Google Analytics tracking code to _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+// src/pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PP2N0DYVYM"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PP2N0DYVYM');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
