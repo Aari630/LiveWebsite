aef4fd99e64283daf9690555cd2032c0c0b4c48f
Aarish <aarisharshi123@gmail.com>
2025-08-07 11:24:01 +0530

    Old look back

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: "class", // 👈 Add this line
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    screens: {
-      '3xl': '1920px', // Ultra-large screens (4K/5K)
-    },
-    container: {
-      center: true,
-      padding: '2rem',
-    },
   },
   plugins: [],
 };
